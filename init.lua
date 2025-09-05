b9172b2974b146baa1d9d4fbd049aa0fb5e916d4
Thomasbush9 <thomasbush52@gmail.com>
2025-09-05 23:58:59 +0200

    lazy git dependency

--- init.lua
+++ init.lua
@@ -1,7 +1,19 @@
 require('keymaps')
 require('options')
 require('plugins.keymaps')
-
+-- add lazy vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 -- init.lua (Neovim config file)
 -- Set up the runtime path for lazy.nvim
 vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
